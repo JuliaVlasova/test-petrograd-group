c9a2c72ffa6207ca92e5310f7298167ec5e7477a
JuliaVlasova <juliavlasova10111989@gmail.com>
2024-09-07 19:02:49 +0300

    desktop pixel perfect

--- src/js/animate-pages.js
+++ src/js/animate-pages.js
@@ -8,14 +8,21 @@ $(document).ready(function() {
     // Управление положением навигации
     function handleHeader() {
         let navHeight = $("nav").outerHeight();
+        let section1Height = $(".section-1").outerHeight();
+        let sectionsHeight = section1Height + $(".section-2").outerHeight() + $(".section-3").outerHeight();
 
         $(window).scroll(function() {
             let scroll = getCurrentScroll();
 
-            if ( scroll >= navHeight ) {
+            if (scroll >= navHeight) {
                 $("nav").addClass("header_absolute");
-            }
-            else {
+
+                if (scroll >= section1Height) {
+                    $("nav").removeClass("header_absolute header_dark").addClass("animate-down header_light");
+                } else {
+                    $("nav").addClass("header_absolute header_dark").removeClass("animate-down header_light");
+                }
+            } else {
                 $("nav").removeClass("header_absolute");
             }
         });
